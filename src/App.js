be992d6f0b20dca1798e9d1f8878882ed26e057a
emilyats <tsemilyanne@gmail.com>
2025-10-21 18:01:07 +0800

    hello

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import BlurText from "./components/BlurText";
+import FaultyTerminal from './components/FaultyTerminal';
+import LiquidEther from './components/LiquidEther';
+import Galaxy from './components/Galaxy';
+import DotGrid from './components/DotGrid';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div style={{ width: '100%', height: '100vh', position: 'relative' }}>
+      <FaultyTerminal
+        scale={2.5}
+        gridMul={[2, 1]}
+        digitSize={1.2}
+        timeScale={0.4}
+        pause={false}
+        scanlineIntensity={0.5}
+        glitchAmount={1}
+        flickerAmount={1}
+        noiseAmp={1}
+        chromaticAberration={0}
+        dither={0}
+        curvature={0.14}
+        tint="#d2a1f2"
+        mouseReact={true}
+        mouseStrength={0.5}
+        pageLoadAnimation={true}
+        brightness={0.6}
+      />
+
+      <nav className="navbar">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#projects">Projects</a>
+        <a href="#contact">Contact</a>
+      </nav>
+
+      <div className="center-text">
+        Hi! I'm Chelsea!
+      </div>
+
     </div>
+
   );
 }
+
 
 export default App;
